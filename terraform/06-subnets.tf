cf113ce57a94d9d27a400b7fa0b4b9e265ddbfc1
timursamanchi <timur.samanchi@gmail.com>
2025-07-24 18:36:55 +0100

    initial commit

--- terraform/06-subnets.tf
+++ terraform/06-subnets.tf
@@ -15,21 +15,3 @@ resource "aws_subnet" "public" {
     Role = "public"
   }
 }
-
-
-#######################################
-# private subnets
-#######################################
-
-resource "aws_subnet" "private" {
-  count             = local.subnet_count
-  vpc_id            = aws_vpc.ecs_vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.ecs_vpc.cidr_block, 4, count.index + local.private_subnet_offset)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "ecs-private-subnet-${count.index + 1}"
-    AZ   = data.aws_availability_zones.available.names[count.index]
-    Role = "private"
-  }
-}
--- terraform/07-routes.tf
+++ terraform/07-routes.tf
@@ -3,14 +3,14 @@
 #######################################
 resource "aws_route_table" "public" {
   # Route table for public subnets: routes 0.0.0.0/0 via internet gateway
-  vpc_id = aws_vpc.k0s.id
+  vpc_id = aws_vpc.ecs_vpc.id
   tags = {
-    Name = "k0s-public-route-table"
+    Name = "${var.vpc_name}-route-table"
   }
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.k0s.id
+    gateway_id = aws_internet_gateway.ecs-gateway.id
   }
 }
 
@@ -19,28 +19,4 @@ resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
-}
-
-
-#######################################
-# Private Subnets Route Table and route table associations 
-#######################################
-resource "aws_route_table" "private" {
-  # Route table for private subnets: routes 0.0.0.0/0 via NAT gateway for outbound internet
-  vpc_id = aws_vpc.k0s.id
-  tags = {
-    Name = "k0s-private-route-table"
-  }
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.k0s_nat.id
-  }
-}
-
-resource "aws_route_table_association" "controller_priv" {
-  # Associates private controller subnets with the private route table
-  count          = length(aws_subnet.controller_priv)
-  subnet_id      = aws_subnet.controller_priv[count.index].id
-  route_table_id = aws_route_table.private.id
 }
