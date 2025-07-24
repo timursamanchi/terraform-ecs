4305eb74824be180bcd89e76cdb5d93d9b08cf07
timursamanchi <timur.samanchi@gmail.com>
2025-07-24 20:56:50 +0100

    initial commit

--- terraform/12-healthcheck.tf
+++ terraform/12-healthcheck.tf
@@ -1,11 +1,15 @@
-resource "null_resource" "ecs_health_test" {
-    
-  depends_on = [
-    aws_ecs_service.quote_backend_service,
-    aws_ecs_service.quote_frontend_service
-  ]
+# resource "null_resource" "ecs_health_test" {
+#   depends_on = [
+#     aws_ecs_service.quote_backend_service,
+#     aws_ecs_service.quote_frontend_service
+#   ]
 
-  provisioner "local-exec" {
-    command = "./scripts/test-ecs-health.sh"
-  }
-}
+#   triggers = {
+#     backend_task_def  = aws_ecs_task_definition.quote_backend.revision
+#     frontend_task_def = aws_ecs_task_definition.quote_frontend.revision
+#   }
+
+#   provisioner "local-exec" {
+#     command = "../scripts/healthcheck-test.sh"
+#   }
+# }
