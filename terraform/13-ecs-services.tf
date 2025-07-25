efa2354d675c5414085202d9221fabaa7a7c8040
timursamanchi <timur.samanchi@gmail.com>
2025-07-26 00:54:43 +0100

    where do I go from here

--- terraform/13-ecs-services.tf
+++ terraform/13-ecs-services.tf
@@ -1,5 +1,5 @@
 #######################################
-# ECS Backend Service (private)
+# ECS Frontend Service (Public, No ALB)
 #######################################
 resource "aws_ecs_service" "quote_frontend_service" {
   name                                = "quote-frontend-service"
@@ -12,7 +12,7 @@ resource "aws_ecs_service" "quote_frontend_service" {
   deployment_maximum_percent         = 200
 
   network_configuration {
-    subnets          = [aws_subnet.ecs-public.id]
+    subnets          = [aws_subnet.ecs-public.id]  # Public subnet
     security_groups  = [aws_security_group.ecs_cluster_sg.id]
     assign_public_ip = true
   }
@@ -25,12 +25,12 @@ resource "aws_ecs_service" "quote_frontend_service" {
 }
 
 #######################################
-# ECS Frontend Service (public via ALB)
+# ECS Backend Service (Private)
 #######################################
-resource "aws_ecs_service" "quote_frontend_service" {
-  name                                = "quote-frontend-service"
+resource "aws_ecs_service" "quote_backend_service" {
+  name                                = "quote-backend-service"
   cluster                             = aws_ecs_cluster.quote_cluster.id
-  task_definition                     = aws_ecs_task_definition.quote_frontend.arn
+  task_definition                     = aws_ecs_task_definition.quote_backend.arn
   desired_count                       = 1
   launch_type                         = "FARGATE"
   scheduling_strategy                 = "REPLICA"
@@ -43,18 +43,9 @@ resource "aws_ecs_service" "quote_frontend_service" {
     assign_public_ip = false
   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.ingress_tg.arn
-    container_name   = "quote-frontend"
-    container_port   = 80
-  }
-
-  depends_on = [
-    aws_lb_listener.ingress_http,
-    aws_iam_role.ecs_task_execution_role
-  ]
-
   tags = {
-    Name = "quote-frontend-service"
+    Name = "quote-backend-service"
   }
+
+  depends_on = [aws_iam_role.ecs_task_execution_role]
 }
